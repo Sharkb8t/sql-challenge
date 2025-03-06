987b07d07e58fc5f71513035a63386b8ad94b176
Sharkb8t <daltonaschmidt@gmail.com>
2025-03-06 18:15:41 -0500

    Updated schema files to properly incorporate all columns from CSV files.

--- EmployeeSQL/Schema/Pewlett Hackard Schema - Conceptual.sql
+++ EmployeeSQL/Schema/Pewlett Hackard Schema - Conceptual.sql
@@ -9,6 +9,7 @@ BEGIN TRANSACTION QUICKDBD
 
 CREATE TABLE [employees] (
     [emp_no] INT  NOT NULL ,
+    [emp_title_id] VARCHAR(10)  NOT NULL ,
     [birth_date] DATE  NOT NULL ,
     [first_name] VARCHAR(50)  NOT NULL ,
     [last_name] VARCHAR(50)  NOT NULL ,
@@ -61,14 +62,17 @@ CREATE TABLE [salaries] (
 )
 
 CREATE TABLE [titles] (
-    [emp_no] INT  NOT NULL ,
+    [title_id] VARCHAR(10)  NOT NULL ,
     [title] VARCHAR(50)  NOT NULL ,
-    [from_date] DATE  NOT NULL ,
-    [to_date] DATE  NOT NULL ,
     CONSTRAINT [PK_titles] PRIMARY KEY CLUSTERED (
-        [emp_no] ASC
+        [title_id] ASC
     )
 )
+
+ALTER TABLE [employees] WITH CHECK ADD CONSTRAINT [FK_employees_emp_title_id] FOREIGN KEY([emp_title_id])
+REFERENCES [titles] ([title_id])
+
+ALTER TABLE [employees] CHECK CONSTRAINT [FK_employees_emp_title_id]
 
 ALTER TABLE [dept_emp] WITH CHECK ADD CONSTRAINT [FK_dept_emp_emp_no] FOREIGN KEY([emp_no])
 REFERENCES [employees] ([emp_no])
@@ -94,10 +98,5 @@ ALTER TABLE [salaries] WITH CHECK ADD CONSTRAINT [FK_salaries_emp_no] FOREIGN KE
 REFERENCES [employees] ([emp_no])
 
 ALTER TABLE [salaries] CHECK CONSTRAINT [FK_salaries_emp_no]
-
-ALTER TABLE [titles] WITH CHECK ADD CONSTRAINT [FK_titles_emp_no] FOREIGN KEY([emp_no])
-REFERENCES [employees] ([emp_no])
-
-ALTER TABLE [titles] CHECK CONSTRAINT [FK_titles_emp_no]
 
 COMMIT TRANSACTION QUICKDBD
